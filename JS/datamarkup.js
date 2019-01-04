277e31b912925d87a4a04bf3fff9741eb52965b4
Hrithik Shah <hrithikchamp@gmail.com>
2019-01-04 01:50:55 -0500

    Organization
    Organized some things

---
+++ JS/datamarkup.js
@@ -0,0 +1,20 @@
+{
+	  "@context" : "http://schema.org",
+	  "@type" : "Article",
+	  "name" : "Hrithik Shah",
+	  "image" : "http://www.googleusercontent.com/images/HrithikShah.jpg",
+	  "articleSection" : "Aspiring Software Engineer",
+	  "author" : {
+    		"@type" : "Person",
+    		"name" : "Hrithik Shah"
+  		},
+  			"publisher" : {
+   			"@type" : "Brand",
+    		"name" : "Hrithik Shah",
+    		"logo" : "https://hrithikshah.com/images/HrithikShah.jpg"
+  		},
+	  	"datePublished" : "June 26, 2017",
+	  	"headline" : "Aspiring Software Engineer",
+	  	"dateModified" : "December 23, 2018",
+	  	"mainEntityOfPage" : "Hrithik Shah"
+}
